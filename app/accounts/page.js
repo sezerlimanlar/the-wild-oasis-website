ba6e592b0b367767c2deb72040c6f420ce8818ef
Sezer <limanlarsezer@gmail.com>
2024-11-05 01:37:58 +0300

    nextjs

---
+++ app/accounts/page.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Accounts Page</h1>
+    </div>
+  );
+}
