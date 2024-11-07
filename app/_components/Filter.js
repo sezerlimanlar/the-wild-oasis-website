d39b8622e33145b4f06f86cac9498bb17ce87344
Sezer <limanlarsezer@gmail.com>
2024-11-07 23:34:01 +0300

    server components in client components

--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -8,6 +8,8 @@ export default function Filter() {
   const router = useRouter();
   const pathname = usePathname();
 
+  const activeFilter = searchParams.get("capacity") ?? "all";
+
   function handleFilter(filter) {
     const params = new URLSearchParams(searchParams);
     params.set("capacity", filter);
@@ -15,30 +17,46 @@ export default function Filter() {
   }
   return (
     <div className="border border-primary-800 flex">
-      <button
-        onClick={() => handleFilter("all")}
-        className="px-5 py-2 hover:bg-primary-700"
+      <Button
+        filter="all"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
       >
         All Cabins
-      </button>
-      <button
-        onClick={() => handleFilter("small")}
-        className="px-5 py-2 hover:bg-primary-700"
+      </Button>
+      <Button
+        filter="small"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
       >
         1&mdash;3 guests
-      </button>
-      <button
-        onClick={() => handleFilter("medium")}
-        className="px-5 py-2 hover:bg-primary-700"
+      </Button>
+      <Button
+        filter="medium"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
       >
         4&mdash;7 guests
-      </button>
-      <button
-        onClick={() => handleFilter("large")}
-        className="px-5 py-2 hover:bg-primary-700"
+      </Button>
+
+      <Button
+        filter="large"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
       >
         8&mdash;12 guests
-      </button>
+      </Button>
     </div>
   );
+}
+
+function Button({ filter, handleFilter, activeFilter, children }) {
+  return <button
+    onClick={() => handleFilter(filter)}
+    className={`px-5 py-2 hover:bg-primary-700 ${
+      activeFilter === filter ? "bg-primary-800" : ""
+    }`}
+  >
+    {children}
+  </button>;
 }
