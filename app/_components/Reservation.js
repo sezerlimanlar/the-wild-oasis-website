9eaffcb48810f1a11151f8a76d55efeae6b2b290
Sezer <limanlarsezer@gmail.com>
2024-11-08 05:18:55 +0300

    reservations

---
+++ app/_components/Reservation.js
@@ -0,0 +1,21 @@
+import React from "react";
+import DateSelector from "./DateSelector";
+import ReservationForm from "./ReservationForm";
+import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
+
+export default async function Reservation({ cabin }) {
+  const [settings, bookedDates] = await Promise.all([
+    getSettings(),
+    getBookedDatesByCabinId(cabin.id),
+  ]);
+  return (
+    <div className="grid grid-cols-2 border border-primary-700 p-2">
+      <DateSelector
+        cabin={cabin}
+        settings={settings}
+        bookedDates={bookedDates}
+      />
+      <ReservationForm cabin={cabin} />
+    </div>
+  );
+}
--- app/cabins/page.js
+++ app/cabins/page.js
@@ -3,6 +3,7 @@ import { getCabins } from "../_lib/data-service";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
 import Filter from "../_components/Filter";
+import ReservationReminder from "../_components/ReservationReminder";
 
 export const metadata = {
   title: "Cabins",
@@ -29,6 +30,7 @@ export default async function Page({ searchParams }) {
       </div>
       <Suspense fallback={<Spinner />} key={filter}>
         <CabinList filter={filter} />
+        <ReservationReminder/>
       </Suspense>
     </div>
   );
